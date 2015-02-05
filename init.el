d0428e1206504d11b8d0710d3f837aecdac53625
Tarmo Aidantausta <tarmo@hiutale.org>
2015-02-05 10:41:41 +0200

    Add libraries to load-path

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 (setq conf-dir
       (expand-file-name "conf" user-emacs-directory))
+(setq lib-dir
+      (expand-file-name "lib" user-emacs-directory))
+
+(add-to-list 'load-path lib-dir)
+
+(let ((default-directory lib-dir))
+  (normal-top-level-add-subdirs-to-load-path))
 
 (add-to-list 'load-path conf-dir)
 
@@ -8,5 +15,6 @@
 (require 'mac-keys-conf)
 (require 'backups-conf)
 (require 'sane-defaults-conf)
+
 (require 'helm-conf)
 (require 'browse-kill-ring-conf)

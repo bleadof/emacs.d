8d2b92d3c503b8e019f274d3974a5a80620a8cb9
Tarmo Aidantausta <tarmo@hiutale.org>
2015-02-11 14:46:48 +0200

    Really fix the OS X shell PATH issue exec-path-from-shell package

--- conf/path/osx-path-conf.el
+++ conf/path/osx-path-conf.el
@@ -1,10 +1,5 @@
-(defun setup-osx-path ()
-  "Map mac keys"
-  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
-  (setq exec-path
-        '("/usr/local/bin" "/usr/bin")))
-
-(if (eq system-type 'darwin)
-    (setup-osx-path))
+(when (memq window-system '(mac ns))
+  (require 'exec-path-from-shell)
+  (exec-path-from-shell-initialize))
 
 (provide 'osx-path-conf)

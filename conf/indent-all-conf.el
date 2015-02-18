2d8876ee5dc842b87659a70a0f25cf8714d0f247
Tarmo Aidantausta <tarmo@hiutale.org>
2015-02-18 11:16:57 +0200

    Add indent all with C-c i

---
+++ conf/indent-all-conf.el
@@ -0,0 +1,4 @@
+(require 'indent-all)
+
+(global-set-key (kbd "C-c i") 'indent-all)
+(provide 'indent-all-conf)
---
+++ lib/indent-all.el
@@ -0,0 +1,8 @@
+(defun indent-all ()
+  "indent whole buffer"
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
+
+(provide 'indent-all)

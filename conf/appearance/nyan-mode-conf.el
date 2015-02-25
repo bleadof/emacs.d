4230dd6455fca407c019155ff395c72579d7d75a
Tarmo Aidantausta <tarmo@hiutale.org>
2015-02-25 21:45:17 +0200

    nyan-mode-conf to use-package

--- conf/appearance/nyan-mode-conf.el
+++ conf/appearance/nyan-mode-conf.el
@@ -1,12 +1,11 @@
 ;; Stolen from https://github.com/cemerick/.emacs.d#nyan-mode
-
-(require 'nyan-mode)
-
-(defun toggle-nyan-mode (&optional frame)
-  (if (display-graphic-p frame)
-      (nyan-mode t)
-    (nyan-mode -1)))
-(add-hook 'after-init-hook 'toggle-nyan-mode)
-(add-hook 'after-make-frame-functions 'toggle-nyan-mode)
+(use-package nyan-mode
+  :init
+  (defun toggle-nyan-mode (&optional frame)
+    (if (display-graphic-p frame)
+        (nyan-mode t)
+      (nyan-mode -1)))
+  (add-hook 'after-init-hook 'toggle-nyan-mode)
+  (add-hook 'after-make-frame-functions 'toggle-nyan-mode))
 
 (provide 'nyan-mode-conf)

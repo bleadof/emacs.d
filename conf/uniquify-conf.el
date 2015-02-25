33c658d858016a5f53d4e663d84e98de180ce5d5
Tarmo Aidantausta <tarmo@hiutale.org>
2015-02-25 21:38:13 +0200

    uniquify to use-package

--- conf/uniquify-conf.el
+++ conf/uniquify-conf.el
@@ -1,9 +1,8 @@
-;; File path to buffer name
-(require 'uniquify)
-
-(setq uniquify-buffer-name-style 'forward)
-(setq uniquify-separator "/")
-(setq uniquify-after-kill-buffer-p t)
-(setq uniquify-ignore-buffers-re "^\\*")
+(use-package uniquify
+  :config
+  (setq uniquify-buffer-name-style 'forward)
+  (setq uniquify-separator "/")
+  (setq uniquify-after-kill-buffer-p t)
+  (setq uniquify-ignore-buffers-re "^\\*"))
 
 (provide 'uniquify-conf)

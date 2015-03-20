74509b114299072796e805471e6312dc8a2bff6c
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2015-03-20 13:18:01 +0200

    Bind goto-line to M-s-l

--- conf/keybindings/general-key-bindings.el
+++ conf/keybindings/general-key-bindings.el
@@ -1,5 +1,6 @@
 (progn
   (bind-key "C-x C-e" 'eval-region)
-  (bind-key "C-c r" 'revert-buffer))
+  (bind-key "C-c r" 'revert-buffer)
+  (bind-key "M-s-l" 'goto-line))
 
 (provide 'general-key-bindings)

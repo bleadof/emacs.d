1dcdd9621bcdcc4c6bcb485a703135dc4ceaba05
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2015-04-18 19:19:37 +0300

    Show trailing white-space

--- conf/sane-defaults-conf.el
+++ conf/sane-defaults-conf.el
@@ -1,7 +1,7 @@
 (progn
   ;; Answering just 'y' or 'n' will do
   (defalias 'yes-or-no-p 'y-or-n-p)
-  
+
   (show-paren-mode 1)
   (setq-default indent-tabs-mode nil)
   (setq x-select-enable-clipboard t
@@ -10,8 +10,8 @@
 	apropos-do-all t
 	mouse-yank-at-point t
 	require-final-newline t
-	visible-bell t))
-
-(desktop-save-mode 1)
+	visible-bell t)
+  (desktop-save-mode 1)
+  (setq show-trailing-whitespace t))
 
 (provide 'sane-defaults-conf)

12ae8498c5276078ad75f7711b861258fe4aea36
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2015-04-29 22:02:33 +0200

    Another stab at backup conf

--- conf/backups-conf.el
+++ conf/backups-conf.el
@@ -1,5 +1,11 @@
 (progn
-  (setq backup-directory-alist `(("." . ,(concat user-emacs-directory
-                                                 "backups")))))
+  (setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+   '(("." . "~/.emacs.d/backups"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t))       ; use versioned backups
 
 (provide 'backups-conf)

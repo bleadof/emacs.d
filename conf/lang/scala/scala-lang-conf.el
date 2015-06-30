2467268044d92aa10a95b3738ca459e1f5317b23
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2015-06-30 11:01:45 +0300

    Use web-mode for Twirl templates

--- conf/lang/scala/scala-lang-conf.el
+++ conf/lang/scala/scala-lang-conf.el
@@ -2,4 +2,11 @@
 (use-package ensime
   :config (add-hook 'scala-mode-hook 'ensime-scala-mode-hook))
 
+(use-package web-mode
+  :config
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.scala\\.html\\'" . web-mode))
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-code-indent-offset 2)))
+
 (provide 'scala-lang-conf)

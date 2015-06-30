96d55d92074dcce068bd0654aa3676b8bd9a2a8e
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2015-06-30 10:49:58 +0300

    Make sure helm works as I like it...

--- conf/fundamentals/helm-conf.el
+++ conf/fundamentals/helm-conf.el
@@ -1,9 +1,13 @@
 (use-package helm
   :bind ("M-i" . helm-imenu)
-  :init
-  (require 'helm-config)
   :config
-  ((helm-mode 1)
-   (helm-autoresize-mode 1)))
+  (progn
+    (use-package helm-config)
+    (setq helm-buffers-fuzzy-matching           t
+          helm-move-to-line-cycle-in-source     t
+          helm-ff-search-library-in-sexp        t
+          helm-ff-file-name-history-use-recentf t)
+    (helm-autoresize-mode 1)
+    (helm-mode 1)))
 
 (provide 'helm-conf)

2baffd8e46009adf436204ad58e826aec89d64ec
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2015-10-15 17:11:46 +0300

    Setup ediff colors

--- conf/fundamentals/ediff-conf.el
+++ conf/fundamentals/ediff-conf.el
@@ -1,6 +1,10 @@
 (use-package ediff
   :config
   (progn
-    (setq ediff-split-window-function 'split-window-horizontally)))
+    (setq ediff-split-window-function 'split-window-horizontally)
+    (ediff-even-diff-A ((((class color) (background dark)) (:background "dark red"))))
+    (ediff-even-diff-B ((((class color) (background dark)) (:background "dark green"))))
+    (ediff-odd-diff-A ((((class color) (background dark)) (:background "dark red"))))
+    (ediff-odd-diff-B ((((class color) (background dark)) (:background "dark green"))))))
 
 (provide 'ediff-conf)

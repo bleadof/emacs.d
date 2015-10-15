ef7fbf7e3f90ab1c8d987678a0d3d5a65c7e3ed5
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2015-10-15 17:15:11 +0300

    Magit conf

--- conf/fundamentals/magit-conf.el
+++ conf/fundamentals/magit-conf.el
@@ -1,5 +1,7 @@
 (use-package magit
   :bind ("M-g" . magit-status)
-  :config (setq magit-last-seen-setup-instructions "1.4.0"))
+  :config (progn
+            (setq magit-last-seen-setup-instructions "1.4.0")
+            (use-package rebase-mode)))
 
 (provide 'magit-conf)

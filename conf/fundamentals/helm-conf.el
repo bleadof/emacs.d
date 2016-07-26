5e816e78a3e1b25474d5d511b19227fa027e0488
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2016-07-26 23:32:52 +0300

    Helm conf

--- conf/fundamentals/helm-conf.el
+++ conf/fundamentals/helm-conf.el
@@ -1,13 +1,21 @@
 (req-package helm
-  :bind (("M-i" . helm-imenu)
-         ("M-x" . helm-M-x))
+  :bind (("C-x C-f" . helm-find-files)
+         ("M-i"     . helm-imenu)
+         ("M-x"     . helm-M-x))
   :config
   (progn
-    (req-package helm-config)
+    (require helm-config)
     (setq helm-buffers-fuzzy-matching           t
           helm-move-to-line-cycle-in-source     t
           helm-ff-search-library-in-sexp        t
-          helm-ff-file-name-history-use-recentf t)
+          helm-ff-file-name-history-use-recentf t
+          helm-find-files                       t
+          helm-reuse-last-window-split-state    nil
+          helm-split-window-in-side-p           nil
+          helm-always-two-windows               t
+          helm-autoresize-max-height            40 ; it is %.
+          helm-autoresize-min-height            10 ; it is %.
+          fit-window-to-buffer-horizontally     1)
     (helm-autoresize-mode 1)
     (helm-mode 1)))
 

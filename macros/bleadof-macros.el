78396a7fcd821c3a7b7bf2e24510ca47889523c5
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2017-07-28 14:31:13 +0300

    Fix bleadof-macros init loading

--- macros/bleadof-macros.el
+++ macros/bleadof-macros.el
@@ -5,4 +5,4 @@
         (kmacro-exec-ring-item
          (quote ([return 1 67108896 25 24 24 134217848 97 108 105 103 110 45 114 101 103 101 120 112 return 61 return 24 24 134217847] 0 "%d")) arg)))
 
-(provide 'bleadof/macros)
+(provide 'bleadof-macros)

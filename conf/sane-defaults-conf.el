888ccc68ec4fe44c5b0359f6e31e37abf5342823
Tarmo Aidantausta <tarmo.aidantausta@veikkaus.fi>
2018-02-20 17:06:03 +0200

    Reformat sane defaults conf

--- conf/sane-defaults-conf.el
+++ conf/sane-defaults-conf.el
@@ -4,17 +4,17 @@
 
   (show-paren-mode 1)
   (setq-default indent-tabs-mode nil)
-  (setq x-select-enable-clipboard t
-	x-select-enable-primary t
-	save-interprogram-paste-before-kill t
-	apropos-do-all t
-	mouse-yank-at-point t
-	require-final-newline t
-	visible-bell t)
   (setq-default case-fold-search nil)
+  (setq x-select-enable-clipboard           t
+        x-select-enable-primary             t
+        save-interprogram-paste-before-kill t
+        apropos-do-all                      t
+        mouse-yank-at-point                 t
+        require-final-newline               t
+        visible-bell                        t
+        show-trailing-whitespace            t
+        max-lisp-eval-depth                 10000)
   (desktop-save-mode 1)
-  (setq show-trailing-whitespace t)
-  (setq max-lisp-eval-depth 10000)
   (put 'upcase-region 'disabled nil))
 
 (provide 'sane-defaults-conf)

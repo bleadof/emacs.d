5815910e228d7ed1328988e6552f491170632e1a
Tarmo Aidantausta <tarmo.aidantausta@veikkaus.fi>
2018-02-20 17:06:44 +0200

    Setup window keybindings that work with Gnome Shell

--- conf/keybindings/window-keybindings-conf.el
+++ conf/keybindings/window-keybindings-conf.el
@@ -3,6 +3,13 @@
   (("s-<left>" . windmove-left)
    ("s-<right>" . windmove-right)
    ("s-<up>" . windmove-up)
-   ("s-<down>" . windmove-down)))
+   ("s-<down>" . windmove-down)
+   ("M-S-<up>" . windmove-up)
+   ("M-S-<down>" . windmove-down)
+   ("M-S-<left>" . windmove-left)
+   ("M-S-<right>" . windmove-right)))
+
+(bind-key "s->" 'enlarge-window-horizontally)
+(bind-key "s-<" 'shrink-window-horizontally)
 
 (provide 'window-keybindings-conf)

25f2eb1e87e13b0354ee229e4c72325340fb533f
Tarmo Aidantausta <tarmo.aidantausta@veikkaus.fi>
2018-02-20 17:01:43 +0200

    Hiili device font config

--- conf/appearance/font-conf.el
+++ conf/appearance/font-conf.el
@@ -1,3 +1,6 @@
-(set-face-attribute 'default nil :height 150)
+(set-face-attribute 'default nil :height
+                    (cond
+                     ((string-equal (system-name) "hiili") 140)
+                     ((t 150))))
 
 (provide 'font-conf)

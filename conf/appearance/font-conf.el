c71cb0ba0db9f88e8658d3ad990818c90cdf7919
Tarmo Aidantausta <tarmo@hiutale.org>
2018-03-02 17:12:44 +0200

    Fix default font-config

--- conf/appearance/font-conf.el
+++ conf/appearance/font-conf.el
@@ -1,6 +1,6 @@
 (set-face-attribute 'default nil :height
                     (cond
                      ((string-equal (system-name) "hiili") 140)
-                     ((t 150))))
+                     (t 150)))
 
 (provide 'font-conf)

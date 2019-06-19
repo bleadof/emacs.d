17d15aa9e43d63e9c169c7df29ff2fc125ce9768
Tarmo Aidantausta <tarmo.aidantausta@firstbeat.com>
2019-06-19 20:35:01 +0300

    fix: projectile to use cache

--- conf/fundamentals/projectile-conf.el
+++ conf/fundamentals/projectile-conf.el
@@ -1,6 +1,9 @@
 (req-package projectile
   :bind ("C-c p" . projectile-command-map)
   :config
-  (projectile-global-mode))
+  (progn
+    (setq projectile-indexing-method 'alien)
+    (setq projectile-enable-caching t)
+    (projectile-global-mode)))
 
 (provide 'projectile-conf)

589c2972072ce746f8b6ab0d4e3ac04fc7a775f2
Tarmo Aidantausta <tarmoaidantausta@magnesium.local>
2022-01-20 16:51:11 +0200

    refactor(conf/fundamentals): projectile -> project

---
+++ conf/fundamentals/project-conf.el
@@ -0,0 +1,5 @@
+(use-package project
+  :ensure t
+  :bind ("C-c p f" . project-find-file))
+
+(provide 'project-conf)
--- conf/fundamentals/projectile-conf.el
+++
@@ -1,8 +0,0 @@
-(use-package projectile
-  :ensure t
-  :bind ("C-c p" . projectile-command-map)
-  :config
-  (setq projectile-indexing-method 'alien)
-  (setq projectile-enable-caching t))
-
-(provide 'projectile-conf)
